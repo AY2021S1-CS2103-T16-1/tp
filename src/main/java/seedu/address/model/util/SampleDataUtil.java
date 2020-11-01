75c10a5491399b9472e3077d5e914efa055b748d
Lim Wei Quan, Ernest <ernestlim8@gmail.com>
2020-11-02 00:53:58 +0800

    Update vendor menus

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -45,30 +45,51 @@ public class SampleDataUtil {
         menu1.add(new Food("Milo Godzilla", 3, new HashSet<>()));
 
 
+
+
         Menu menu2 = new Menu();
         HashSet<Tag> tagList = new HashSet<>();
         tagList.add(new Tag("Egg"));
         tagList.add(new Tag("Cheese"));
-        menu2.add(new Food("Prata", 1.0, tagList));
-        menu2.add(new Food("Water", 3, new HashSet<>()));
+        menu2.add(new Food("Tandoori", 6.50, new HashSet<>()));
+        menu2.add(new Food("Mutton Briyani", 7.50, new HashSet<>()));
+        menu2.add(new Food("Prata Plain", 1.2, new HashSet<>()));
+        menu2.add(new Food("Prata Egg",1.7, new HashSet<>()));
+        menu2.add(new Food("Milo Cold", 2, new HashSet<>()));
+        menu2.add(new Food("Nasi Goreng Ikan Bilis",4.5, new HashSet<>()));
+        menu2.add(new Food("Roti John Chicken",5.5, new HashSet<>()));
+        menu2.add(new Food("Beef Pattaya",6.5, new HashSet<>()));
+        menu2.add(new Food("Cheese Fries",4, new HashSet<>()));
+        menu2.add(new Food("Plain Naan",1.7, new HashSet<>()));
+        menu2.add(new Food("Butter Chicken Masala", 7.5, getTagSet("Small")));
+        menu2.add(new Food("Butter Chicken Masala", 11, getTagSet("Large")));
+
+
         Menu menu3 = new Menu();
         HashSet<Tag> tagList2 = new HashSet<>();
         tagList2.add(new Tag("No ice"));
-        menu3.add(new Food("Steak", 16.90, new HashSet<>()));
-        menu3.add(new Food("Hamburger", 4.20, new HashSet<>()));
-        menu3.add(new Food("Lemon tea", 1.2, tagList2));
+        menu3.add(new Food("Foie Gras Sliced Beef Roll", 18.90, new HashSet<>()));
+        menu3.add(new Food("Mozzarella Corn Dog", 12.90, getTagSet("2 Piecess")));
+        menu3.add(new Food("Prata Plain", 1.2, new HashSet<>()));
+        menu3.add(new Food("Sausage Marinara Spaghetti",13.90, new HashSet<>()));
+        menu3.add(new Food("Fusion Prawn Aglio Olio",13.90, spicyTags));
+        menu3.add(new Food("Mentaiko Slide Beef Rice",7.90, spicyTags));
+        menu3.add(new Food("Teriyaki Chicken Rice",7.90, spicyTags));
+        menu3.add(new Food("Spicy Coney Rice",7.90, spicyTags));
+        menu3.add(new Food("Japanese Kaarage Rice",7.90, spicyTags));
+        menu3.add(new Food("Japanese Ebi Rice",7.90, spicyTags));
+
 
         return new Vendor[]{
             new Vendor(new Name("Al Amaan Restaurant"), new Phone("67770555"),
                         new Email("alamaanrestaurant@gmail.com"),
                         new Address("12 Clementi Road, Singapore 129742"),
                         getTagSet("halal"), menu1),
-            new Vendor(new Name("Local Indian Store"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                        new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                        getTagSet("colleagues", "friends"), menu2),
-            new Vendor(new Name("Ameens"), new Phone("93210283"), new Email("charlotte@example.com"),
-                        new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                        getTagSet("neighbours"), menu3)
+            new Vendor(new Name("Makan Express"), new Phone("91076367"), new Email("xpressmakanavenue@gmail.com"),
+                        new Address("14 Clementi Roadd, Singapore 129743"),
+                    getTagSet("halal"), menu2),
+            new Vendor(new Name("Be Frank"), new Phone("97652509"), new Email("befrank@gmail.com"),
+                        new Address("28 Clementi Road, Singapore 129754"), new HashSet<>(), menu3)
         };
     }
 
